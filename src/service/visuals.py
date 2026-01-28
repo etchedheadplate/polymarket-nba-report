4f2954efff3fb55ed12c5bcbc8efe81b8a4e41d4
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-28 22:52:21 +0300

    Created visuals module

---
+++ src/service/visuals.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+from src.core.visuals import Plot
+from src.service.domain import NBATeamColor
+from src.service.schemas import GamesSeriesResponse
+
+
+class GamesPlot(Plot):
+    def __init__(self, games_data: dict[int, GamesSeriesResponse]) -> None:
+        super().__init__(games_data)
+
+    def _make_transparent_image(self) -> list[tuple[Path, Path]]:
+        visuals_paths: list[tuple[Path, Path]] = []
+
+        for game_id, game_data in self._input_data.items():
+
+            guest_series = [(p.timestamp, p.guest_price) for p in game_data.prices if p.guest_price is not None]
+            host_series = [(p.timestamp, p.host_price) for p in game_data.prices if p.host_price is not None]
+
+            guest_team = game_data.guest_team
+            host_team = game_data.host_team
+            guest_score = game_data.guest_score
+            host_score = game_data.host_score
+
+            guest_color_scheme = getattr(NBATeamColor, guest_team, None)
+            guest_color = guest_color_scheme["guest"] if guest_color_scheme else "#6c757d"
+            host_color_scheme = getattr(NBATeamColor, host_team, None)
+            host_color = host_color_scheme["host"] if host_color_scheme else "#ced4da"
+
+            game_date = game_data.game_date.isoformat()
+            market_type = game_data.market_type
+
+            path_without_bg = self._plot_dir / f"tmp_{game_id}.png"
+            path_with_bg = self._plot_dir / f"{game_date}_{guest_team}_{host_team}_{game_id}_{market_type}.png"
+
+            if path_with_bg.exists():
+                continue
+
+            if not guest_series and not host_series:
+                continue
+
+            with plt.rc_context(rc=self._plot_style):  # type: ignore[reportUnknownMemberType]
+                plt.figure(figsize=(10, 5))  # type: ignore[reportUnknownMemberType]
+
+                if guest_series:
+                    x, y = zip(*guest_series, strict=False)
+                    x = [datetime.fromtimestamp(ts) for ts in x]
+                    plt.plot(x, y, label=guest_team, color=guest_color, linewidth=5)  # type: ignore[reportUnknownMemberType]
+
+                if host_series:
+                    x, y = zip(*host_series, strict=False)
+                    x = [datetime.fromtimestamp(ts) for ts in x]
+                    plt.plot(x, y, label=host_team, color=host_color, linewidth=5)  # type: ignore[reportUnknownMemberType]
+
+                plt.title(f"{game_date} • {guest_team} {guest_score}:{host_score} {host_team} • {game_id} {market_type}")  # type: ignore[reportUnknownMemberType]
+                plt.xlabel("TIME (UTC)")  # type: ignore[reportUnknownMemberType]
+                plt.ylabel("PRICE, USDC")  # type: ignore[reportUnknownMemberType]
+                plt.grid(True)  # type: ignore[reportUnknownMemberType]
+                plt.legend(facecolor="#f8f9fa", edgecolor="#e9ecef", framealpha=0.15, labelcolor="#e9ecef")  # type: ignore[reportUnknownMemberType]
+                plt.tight_layout()
+                plt.savefig(path_without_bg, transparent=True)  # type: ignore[reportUnknownMemberType]
+                plt.close()
+
+                paths = (path_without_bg, path_with_bg)
+                visuals_paths.append(paths)
+
+        return visuals_paths
