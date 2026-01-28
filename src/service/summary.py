6e00219173835658bdff0623c4416cf654866fb1
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-28 23:43:17 +0300

    Made dummy class

--- src/service/summary.py
+++ src/service/summary.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+
 from src.core.summary import Summary
 
 
-class GamesSummary(Summary): ...
+class GamesSummary(Summary):
+    def _make_data_summary(self) -> Path:
+        return Path()
