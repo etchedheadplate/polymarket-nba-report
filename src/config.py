0997024e9b06c5dd36b033b3e280279ae56f1af2
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-28 22:54:29 +0300

    Updated app config

--- src/config.py
+++ src/config.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from typing import Any
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -15,26 +14,6 @@ class Settings(BaseSettings):
 
     PLOT_FONT_PATH: Path
     PLOT_BACKGROUND_PATH: Path
-    PLOT_CONFIG: dict[str, Any] = {
-        "font.family": "Montserrat",
-        "text.color": "#e9ecef",
-        "figure.facecolor": "#e9ecef",
-        "axes.facecolor": "#e9ecef",
-        "axes.labelcolor": "#e9ecef",
-        "axes.titlecolor": "#e9ecef",
-        "axes.edgecolor": "#bbbbbb",
-        "axes.labelsize": 10,
-        "axes.titlesize": 20,
-        "xtick.labelsize": 12,
-        "ytick.labelsize": 12,
-        "xtick.color": "#e9ecef",
-        "ytick.color": "#e9ecef",
-        "axes.grid": True,
-        "grid.color": "#f8f9fa",
-        "grid.linestyle": "-",
-        "grid.linewidth": 1,
-        "grid.alpha": 0.6,
-    }
 
     model_config = SettingsConfigDict(env_file=".env")
 
