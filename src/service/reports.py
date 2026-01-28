1ce7651fbcd3fe08f6fd36ef58b365a2fd7990db
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-28 22:53:39 +0300

    Created reports module

---
+++ src/service/reports.py
@@ -0,0 +1,65 @@
+from decimal import Decimal
+from typing import Any
+
+from src.core.reports import Report
+from src.database.connection import async_session_maker
+from src.service.repos import NBAGamesRepo
+from src.service.schemas import GamesSeriesPriceResponse, GamesSeriesQuery, GamesSeriesResponse
+from src.service.visuals import GamesPlot
+
+
+class GamesReport(Report):
+    _visuals_cls = GamesPlot
+
+    def __init__(self, query: GamesSeriesQuery) -> None:
+        super().__init__(query)
+
+    async def _query_database(self) -> list[Any]:
+        async with async_session_maker() as session:
+            return await NBAGamesRepo().get_games_series(session, self.query)
+
+    def _process_data(self, raw_data: list[Any]) -> dict[int, GamesSeriesResponse]:
+        games_data_dict: dict[int, GamesSeriesResponse] = {}
+
+        def normalize_prices(buy: Decimal | None, sell: Decimal | None) -> Decimal | None:
+            if buy is not None and sell is not None:
+                return (buy + sell) / 2
+            return buy or sell
+
+        for (
+            game_id,
+            game_date,
+            g_team,
+            h_team,
+            g_score,
+            h_score,
+            market_type,
+            ts,
+            g_buy,
+            g_sell,
+            h_buy,
+            h_sell,
+        ) in raw_data:
+            game_entry = games_data_dict.setdefault(
+                game_id,
+                GamesSeriesResponse(
+                    game_id=game_id,
+                    game_date=game_date,
+                    market_type=market_type,
+                    guest_team=g_team,
+                    host_team=h_team,
+                    guest_score=g_score,
+                    host_score=h_score,
+                    prices=[],
+                ),
+            )
+
+            game_entry.prices.append(
+                GamesSeriesPriceResponse(
+                    timestamp=ts,
+                    guest_price=normalize_prices(g_buy, g_sell),
+                    host_price=normalize_prices(h_buy, h_sell),
+                )
+            )
+
+        return games_data_dict
