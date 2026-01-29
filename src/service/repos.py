0f273c0823aafff962fcaa766f3a90cc6923e8d1
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-29 18:49:37 +0300

    Fixed games limit param

--- src/service/repos.py
+++ src/service/repos.py
@@ -11,6 +11,51 @@ from src.service.schemas import GamesSeriesQuery
 class NBAGamesRepo:
     async def get_games_series(self, session: AsyncSession, query: GamesSeriesQuery) -> list[Any]:
 
+        base_conditions = [
+            NBAGamesModel.game_status == query.game_status,
+            NBAMarketsModel.market_type == query.market_type,
+        ]
+
+        if query.team_side == NBATeamSide.GUEST:
+            base_conditions.append(NBAGamesModel.guest_team == query.team.name)
+        elif query.team_side == NBATeamSide.HOST:
+            base_conditions.append(NBAGamesModel.host_team == query.team.name)
+        else:
+            base_conditions.append(
+                or_(NBAGamesModel.guest_team == query.team.name, NBAGamesModel.host_team == query.team.name)
+            )
+
+        if query.team_vs:
+            if query.team_side == NBATeamSide.GUEST:
+                base_conditions.append(
+                    and_(NBAGamesModel.guest_team == query.team.name, NBAGamesModel.host_team == query.team_vs.name)
+                )
+            elif query.team_side == NBATeamSide.HOST:
+                base_conditions.append(
+                    and_(NBAGamesModel.guest_team == query.team_vs.name, NBAGamesModel.host_team == query.team.name)
+                )
+            else:
+                base_conditions.append(
+                    or_(
+                        and_(
+                            NBAGamesModel.guest_team == query.team.name, NBAGamesModel.host_team == query.team_vs.name
+                        ),
+                        and_(
+                            NBAGamesModel.guest_team == query.team_vs.name, NBAGamesModel.host_team == query.team.name
+                        ),
+                    )
+                )
+
+        games_ids_stmt = (
+            select(NBAGamesModel.id)
+            .join(NBAMarketsModel, NBAMarketsModel.event_id == NBAGamesModel.id)
+            .where(*base_conditions)
+            .order_by(NBAGamesModel.game_date.desc())
+        )
+
+        if query.limit is not None:
+            games_ids_stmt = games_ids_stmt.limit(query.limit)
+
         stmt = (
             select(
                 NBAGamesModel.id,
@@ -26,46 +71,17 @@ class NBAGamesRepo:
                 NBAPricesModel.price_host_buy,
                 NBAPricesModel.price_host_sell,
             )
-            .join(NBAMarketsModel, NBAPricesModel.market_id == NBAMarketsModel.id)
-            .join(NBAGamesModel, NBAMarketsModel.event_id == NBAGamesModel.id)
-        )
-
-        conditions = [NBAGamesModel.game_status == query.game_status, NBAMarketsModel.market_type == query.market_type]
-
-        if query.team_side == NBATeamSide.GUEST:
-            conditions.append(NBAGamesModel.guest_team == query.team.name)
-        elif query.team_side == NBATeamSide.HOST:
-            conditions.append(NBAGamesModel.host_team == query.team.name)
-        elif not query.team_side:
-            conditions.append(
-                or_(NBAGamesModel.guest_team == query.team.name, NBAGamesModel.host_team == query.team.name)
+            .join(NBAMarketsModel, NBAMarketsModel.event_id == NBAGamesModel.id)
+            .join(NBAPricesModel, NBAPricesModel.market_id == NBAMarketsModel.id)
+            .where(
+                NBAGamesModel.id.in_(games_ids_stmt),
+                NBAMarketsModel.market_type == query.market_type,
             )
-
-        if query.team_vs:
-            if query.team_side == NBATeamSide.GUEST:
-                conditions.append(
-                    and_(NBAGamesModel.guest_team == query.team.name, NBAGamesModel.host_team == query.team_vs.name)
-                )
-            elif query.team_side == NBATeamSide.HOST:
-                conditions.append(
-                    and_(NBAGamesModel.guest_team == query.team_vs.name, NBAGamesModel.host_team == query.team.name)
-                )
-            else:
-                conditions.append(
-                    or_(
-                        and_(
-                            NBAGamesModel.guest_team == query.team.name, NBAGamesModel.host_team == query.team_vs.name
-                        ),
-                        and_(
-                            NBAGamesModel.guest_team == query.team_vs.name, NBAGamesModel.host_team == query.team.name
-                        ),
-                    )
-                )
-
-        stmt = stmt.where(*conditions)
-
-        if query.limit is not None:
-            stmt = stmt.limit(query.limit)
+            .order_by(
+                NBAGamesModel.game_date.desc(),
+                NBAPricesModel.timestamp.asc(),
+            )
+        )
 
         result = await session.execute(stmt)
         return list(result.all())
