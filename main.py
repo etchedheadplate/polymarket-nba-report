96da677983414b268b1681325cd6d2f82dffbed2
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-01-31 13:18:54 +0300

    Updated entry point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from src.service.domain import NBATeam, NBATeamSide
+from src.service.domain import NBATeam
 from src.service.reports import QuoteSeriesReport
 from src.service.schemas import GameSeriesQuery
 
@@ -17,7 +17,7 @@ async def generate_all_combinations():
 
 
 async def main():
-    query = GameSeriesQuery(team=NBATeam.NYK, team_vs=NBATeam.TOR, limit=4, team_side=NBATeamSide.GUEST)
+    query = GameSeriesQuery(team=NBATeam.NYK, team_vs=NBATeam.BOS)
     report = QuoteSeriesReport(query)
     await report.make_report()
 
