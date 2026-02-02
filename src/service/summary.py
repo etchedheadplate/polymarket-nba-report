f8dd51c68deb7d22346184cd442639ca74ae0fdd
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-02-02 22:41:02 +0300

    Created init method

--- src/service/summary.py
+++ src/service/summary.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 
 from src.core.summary import Summary
+from src.service.schemas import GameSeries
 
 
-class QuoteSeriesSummary(Summary):
+class GameSeriesSummary(Summary):
+    def __init__(self, games_data: dict[int, GameSeries]) -> None:
+        super().__init__(games_data)
+
     def _make_data_summary(self) -> Path:
         return Path()
