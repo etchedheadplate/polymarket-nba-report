db1a40ac55f09922748b70f1f576a219260ac910
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-02-03 18:39:49 +0300

    Updated query database method

--- src/service/dataset.py
+++ src/service/dataset.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from typing import Any
 
 from src.core.dataset import DataSet
@@ -13,19 +14,25 @@ class GameSeriesDataSet(DataSet):
     def __init__(self, query: Any) -> None:
         super().__init__(query)
 
-    async def query_database(self) -> None:
+    async def query_database(self) -> list[Any]:
         async with async_session_maker() as session:
-            self._rows = await NBAGamesRepo().get_games_series(session, self._query)
+            return await NBAGamesRepo().get_games_series(session, self._query)
 
     async def create_dataset(self) -> dict[int, GameSeries]:
-        await self.query_database()
-        processor = self._processor_cls(self._query, self._rows)
+        rows = await self.query_database()
+        processor = self._processor_cls(rows)
         game_series = processor.create_data_dict()
 
         for _, game in game_series.items():
             halftime_segment = processor.extract_halftime_segment(game)
             game._halftime_ts = halftime_segment  # type: ignore[reportPrivateUsage]
+
             underdog_segments = processor.extract_underdog_segments(game)
             game._underdog_segs = underdog_segments  # type: ignore[reportPrivateUsage]
+
+            price_change_segments = processor.extract_price_change_segments(
+                game, start_price=Decimal("0.1"), end_price=Decimal("0.3")
+            )
+            print(_, price_change_segments)
 
         return game_series
