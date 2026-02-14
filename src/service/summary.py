763d2300af8e0a1163716627df497b4112c0f1df
etchedheadplate <94640304+etchedheadplate@users.noreply.github.com>
2026-02-14 21:46:32 +0300

    Created item schemas for different reports

--- src/service/summary.py
+++ src/service/summary.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 
 from src.core.summary import Summary
-from src.service.schemas import GameData, ReportQuery
+from src.service.schemas import QuoteSeriesItem, ReportQuery
 
 
 class QuoteSeriesSummary(Summary):
-    def __init__(self, query: ReportQuery, dataset: dict[int, GameData]) -> None:
+    def __init__(self, query: ReportQuery, dataset: dict[int, QuoteSeriesItem]) -> None:
         super().__init__(query=query, dataset=dataset)
 
     def _make_data_summary(self) -> Path:
@@ -13,7 +13,7 @@ class QuoteSeriesSummary(Summary):
 
 
 class PriceWindowSummary(Summary):
-    def __init__(self, query: ReportQuery, dataset: dict[int, GameData]) -> None:
+    def __init__(self, query: ReportQuery, dataset: dict[int, QuoteSeriesItem]) -> None:
         super().__init__(query=query, dataset=dataset)
 
     def _make_data_summary(self) -> Path:
